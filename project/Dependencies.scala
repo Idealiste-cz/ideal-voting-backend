e470a848c8c59186013052bf7f40ec2f6f251af5
Scala Steward <me@scala-steward.org>
2021-07-18 04:18:50 +0200

    Update zio-interop-cats to 3.1.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
     val mariadb = "2.7.3"
     val snakeyaml = "1.29"
     val zio = "1.0.9"
-    val zioCats = "2.5.1.0"
+    val zioCats = "3.1.1.0"
     val zioConfig = "1.0.6"
     val zioDoobie = "0.0.4"
     val zioLoggingSlf4j = "0.5.11"
