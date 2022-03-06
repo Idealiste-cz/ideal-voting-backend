73f06dc76e8142a18c0a5be96ed9f01056f6648c
Ondra Pelech <ondra.pelech@gmail.com>
2022-03-06 17:53:22 +0100

    Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,8 +16,6 @@ object Dependencies {
     val mariadb = "2.7.5"
     val snakeyaml = "1.30"
     val zio = "1.0.13"
-    val zioCats = "3.2.9.1"
-    val zioConfig = "1.0.10"
     val zioDoobie = "0.2.0"
     val zioLoggingSlf4j = "0.5.14"
     val zioMagic = "0.3.11"
