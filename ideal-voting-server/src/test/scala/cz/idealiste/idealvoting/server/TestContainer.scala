52db80f56833d782552129c7ecdeb304790d11c7
Ondra Pelech <ondra.pelech@gmail.com>
2024-10-15 01:36:21 +0200

    remove _1 from serviceName everywhere

--- ideal-voting-server/src/test/scala/cz/idealiste/idealvoting/server/TestContainer.scala
+++ ideal-voting-server/src/test/scala/cz/idealiste/idealvoting/server/TestContainer.scala
@@ -14,17 +14,17 @@ object TestContainer {
 
   private[server] lazy val dockerCompose = ZLayer.fromTestContainer {
     new ComposeContainer(new File("docker-compose.yml"))
-      .withExposedService("mariadb_1", 3306)
-      .withExposedService("mailhog_1", 1025)
-      .withExposedService("mailhog_1", 8025)
+      .withExposedService("mariadb", 3306)
+      .withExposedService("mailhog", 1025)
+      .withExposedService("mailhog", 8025)
       .withLocalCompose(false)
   }
 
   private[server] lazy val layer = ZLayer.fromZIO {
     for {
       docker <- ZIO.service[ComposeContainer]
-      host = docker.getServiceHost("mariadb_1", 3306)
-      port = docker.getServicePort("mariadb_1", 3306).toInt
+      host = docker.getServiceHost("mariadb", 3306)
+      port = docker.getServicePort("mariadb", 3306).toInt
       config0 <- ZIO.service[Config]
       config = config0.focus(_.dbTransactor.hikari.jdbcUrl).replace(show"jdbc:mariadb://$host:$port/idealvoting")
       _ <- ZIO.logInfo(s"Modified test configuration:\n${BlackWhite(config)}.")
