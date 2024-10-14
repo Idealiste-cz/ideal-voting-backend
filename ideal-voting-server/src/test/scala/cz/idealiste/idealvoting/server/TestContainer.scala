4850c4c71e7e064ba6ce43453e8c282e2fc24653
Ondra Pelech <ondra.pelech@gmail.com>
2024-10-15 01:41:36 +0200

    localCompose = false

--- ideal-voting-server/src/test/scala/cz/idealiste/idealvoting/server/TestContainer.scala
+++ ideal-voting-server/src/test/scala/cz/idealiste/idealvoting/server/TestContainer.scala
@@ -1,9 +1,9 @@
 package cz.idealiste.idealvoting.server
 
 import cats.implicits._
+import com.dimafeng.testcontainers.{DockerComposeContainer, ExposedService}
 import cz.idealiste.idealvoting.server.Config
 import monocle.syntax.all._
-import org.testcontainers.containers.ComposeContainer
 import pprint.PPrinter.BlackWhite
 import zio._
 import zio.testcontainers._
@@ -13,18 +13,21 @@ import java.io.File
 object TestContainer {
 
   private[server] lazy val dockerCompose = ZLayer.fromTestContainer {
-    new ComposeContainer(new File("docker-compose.yml"))
-      .withExposedService("mariadb", 3306)
-      .withExposedService("mailhog", 1025)
-      .withExposedService("mailhog", 8025)
-      .withLocalCompose(false)
+    new DockerComposeContainer(
+      new File("docker-compose.yml"),
+      List(
+        ExposedService("mariadb", 3306),
+        ExposedService("mailhog", 1025),
+        ExposedService("mailhog", 8025),
+      ),
+      localCompose = false,
+    )
   }
 
   private[server] lazy val layer = ZLayer.fromZIO {
     for {
-      docker <- ZIO.service[ComposeContainer]
-      host = docker.getServiceHost("mariadb", 3306)
-      port = docker.getServicePort("mariadb", 3306).toInt
+      docker <- ZIO.service[DockerComposeContainer]
+      (host, port) <- docker.getHostAndPort("mariadb")(3306)
       config0 <- ZIO.service[Config]
       config = config0.focus(_.dbTransactor.hikari.jdbcUrl).replace(show"jdbc:mariadb://$host:$port/idealvoting")
       _ <- ZIO.logInfo(s"Modified test configuration:\n${BlackWhite(config)}.")
